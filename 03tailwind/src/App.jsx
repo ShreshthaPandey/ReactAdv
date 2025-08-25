3f061ef8ae64d12fdb106c60882712430bbcae9c
ShreshthaPandey <shreshthaP2624@gmail.com>
2025-08-25 21:20:36 +0530

    lec 8

---
+++ 03tailwind/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username: "hitesh",
+    age: 21
+  }
+
+  return (
+    <>
+
+     <h1 className='bg-green-500 text-black'>Tailwind test</h1>
+     <Card username = "helloJi" btnText = "click me"/>
+     <Card username = "chaiAurCode" someObj = {myObj} btnText="visit me"/>
+    </>
+  )
+}
+
+export default App
